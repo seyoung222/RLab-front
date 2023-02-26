5dc827943498cdd6cf115dbbf315eb27b99e3b31
seyoung <kimsyty@naver.com>
2023-02-27 01:59:34 +0900

    [backup] 세미 5페이지 업로드 및 세부오류 수정
    - 스터디 : 헤더fixed적용 및 상단고정, body 중앙배치, footer 위 마진 추가, 배경길이 수정
    - 메인 : 헤더 위치 수정, 이미지크기 수정
    - 모집 : 폴더링에 따른 파일 경로 수정
    - 예약 : footer위 마진추가

---
+++ 세미/js/study.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+    
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      
+      
+    
+    locale: 'ko',
+
+      headerToolbar: {
+        left: '',//dayGridMonth,listMonth
+        center: '',
+        right: 'prev,title,next'
+      },
+      initialDate: '2023-01-12',
+      navLinks: true, // can click day/week names to navigate views
+      editable: true,
+      dayMaxEvents: true, // allow "more" link when too many events
+    
+      events: eventObj
+    
+    });
+    
+    calendar.render();
+  });
+  // 나중에 데이터베이스 값을 가져오기위에 밑에 설정함
+  let eventObj = [
+        {
+          title: '정처기 필기',
+          start: '2023-01-03T12:00:00'
+        },
+
+        {
+        title: '정처기 접수기간',
+        start: '2023-01-10',
+        end: '2023-01-15'
+        },
+        {
+        title: '정처기 실기',
+        start: '2023-01-29T12:00:00'
+        },
+        
+    ]
+
