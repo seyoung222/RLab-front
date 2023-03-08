3f567c534b0716fd652104e23bc14a48c54d161e
jeonghyeon <devhye@naver.com>
2023-03-09 00:12:04 +0900

    [global] - to do list 제이쿼리 추가

--- js/study.js
+++ js/study.js
@@ -43,3 +43,37 @@ document.addEventListener('DOMContentLoaded', function() {
         
     ]
 
+      $(document).ready(function() {
+        $(".add_todo").click(function() {
+          var new_todo = '<div class="to_do_list_contents"><label><input type="checkbox"> <span class="todo-text">새로운 할 일</span></label></div><br>';
+          $(".to_do_list_box1").append(new_todo);
+        });
+    
+      $(".to_do_list_box1").on("click", "input[type=checkbox]", function() {
+        if ($(this).prop("checked")) {
+          $(this).parent().css({"text-decoration": "line-through", "color": "slategray"});
+        } else {
+          $(this).parent().css({"text-decoration": "none", "color": "black"});
+        }
+      });
+  $(".to_do_list_box1").on("dblclick", ".todo-text", function() {
+    var old_text = $(this).text().trim();
+    // contenteditable 속성 추가하여 텍스트 수정 가능하도록 함
+    $(this).attr("contenteditable", true);
+    $(this).focus();
+    // 엔터키를 누르면 텍스트 수정 종료하도록 이벤트 리스너 추가
+    $(this).on("keydown", function(event) {
+      if (event.keyCode === 13) {
+        event.preventDefault();
+        $(this).blur();
+      }
+    });
+    $(this).on("blur", function() {
+      var new_text = $(this).text().trim();
+      $(this).attr("contenteditable", false);
+      if (new_text !== "" && new_text !== old_text) {
+        $(this).text(new_text);
+      }
+    });
+  });
+});
