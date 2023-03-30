6baae8f93ca536d16910426648e6150fb35be2ad
Doltang <doltangdoltang@gmail.com>
2023-03-30 19:37:39 +0900

    [fix] - confirmAction 함수 수정

--- js/management_study.js
+++ js/management_study.js
@@ -15,23 +15,25 @@ $(document).ready(function(){
 
 
 // 버튼 클릭시 confirm창 나타남
+function confirmAction(buttonText, action) {
+  if (confirm(buttonText)) {
+    action();
+	}else{
+		console.log("작업 취소");
+	}
+}
+
 $(".btn_finish").on("click", function() {
-  if (confirm("완료된 스터디로 전환 시 스터디 활동이 일부 제한되며, 자유게시판만 사용가능합니다. 목표를 달성하거나 일정이 모두 완료된 경우에만 전환할 것을 권유드립니다. 정말 완료하시겠습니까?")) {
-    // 사용자가 "확인" 버튼을 클릭한 경우 수행할 작업
-    console.log("완료 실행");
-  } else {
-    // 사용자가 "취소" 버튼을 클릭한 경우 수행할 작업
-    console.log("완료 취소");
-  }
+  confirmAction("완료된 스터디로 전환 시 스터디 활동이 일부 제한되며, 자유게시판만 사용가능합니다. 목표를 달성하거나 일정이 모두 완료된 경우에만 전환할 것을 권유드립니다. 정말 완료하시겠습니까?", function() {
+    alert("스터디가 완료처리 되었습니다.")
+		
+  });
 });
 
 $(".btn_delete").on("click", function() {
-  if (confirm("스터디 삭제 시 스터디에올라온 게시글, 인증내역, 일정, 회원정보 등 모든 정보가 함께 삭제되며 해당 스터디에 접근이 불가합니다. 정말 삭제하시겠습니까?")) {
-    // 사용자가 "확인" 버튼을 클릭한 경우 수행할 작업
-    console.log("삭제 실행");
-  } else {
-		// 사용자가 "취소" 버튼을 클릭한 경우 수행할 작업
-    console.log("삭제 취소");
-  }
+  confirmAction("스터디 삭제 시 스터디에올라온 게시글, 인증내역, 일정, 회원정보 등 모든 정보가 함께 삭제되며 해당 스터디에 접근이 불가합니다. 정말 삭제하시겠습니까?", function() {
+    alert("스터디가 삭제되었습니다.");
+  });
 });
+
 
